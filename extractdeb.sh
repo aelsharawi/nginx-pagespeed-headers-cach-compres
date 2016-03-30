b8fdc0296390ac609872464617aea16e1fdf7bc4
Peter Tonoli <anarchie+git@metaverse.org>
2016-03-30 20:28:59 +1100

    Updated Nginx version to 1.9.13 in extractdeb.sh

--- extractdeb.sh
+++ extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 containerID=$(docker run -d metaverseorg/buildnginx)
-docker cp $containerID:/root/build/nginx-1.9.12/nginx_1.9.13-1_amd64.deb .
+docker cp $containerID:/root/build/nginx-1.9.13/nginx_1.9.13-1_amd64.deb .
 sleep 1
 docker rm $containerID
